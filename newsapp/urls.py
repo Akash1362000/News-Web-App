0c67e95886ee423465fc3b060784e6e4ed922e9b
Samiksha Naik <samikshanaik8888@gmail.com>
2020-09-07 16:29:13 +0530

    Added Sports, Business, Entertainment

--- newsapp/urls.py
+++ newsapp/urls.py
@@ -4,5 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.Home, name='news-home'),
     path('contact-us', views.ContactUs, name='contact-us') 
-    path('feedback',views.Feedback, name='Feedback')
+    path('feedback',views.Feedback, name='feedback')
+    path('forget',views.Forget, name='forget')
+    path('login',views.login, name='login')
+    path('registration',views.registration, name='registration')
+    path('sports',views.Sports, name='sports')
+    path('business',views.Business, name='business')
+    path('entertainment',views.Entertainment, name='entertainment')
 ]
--- newsapp/views.py
+++ newsapp/views.py
@@ -7,3 +7,32 @@ def Home(request):
 
 def ContactUs(request):
     return render(request, 'newsapp/contact_us.html')
+
+
+def Feedback(request):
+    return render(request, 'newsapp/feedbacks.html')
+
+
+def Registration(request):
+    return render(request, 'newsapp/registration.html')
+
+
+def Login(request):
+    return render(request, 'newsapp/login.html')
+
+
+def Forget(request):
+    return render(request, 'newsapp/forget.html')
+
+
+def Sports(request):
+    return render(request, 'newsapp/sports.html')
+
+
+def Entertainment(request):
+    return render(request, 'newsapp/entertainment.html')
+
+
+def Business(request):
+    return render(request, 'newsapp/business.html')
+
