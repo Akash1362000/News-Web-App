e19f7b5c1b1bbdd9587c67117d70583ab5d249c5
Akash Shrivastava <akash.shrivastava136@gmail.com>
2020-09-17 18:53:18 +0530

    Added news fetch api

--- newsapp/views.py
+++ newsapp/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# Importing News API
+from newsapi import NewsApiClient
 
 # Create your views here.
 def Home(request):
-    return render(request, 'newsapp/home.html')
+    newsapi = NewsApiClient(api_key ='492204b5fab24074b7e237e955eb3218')
+    top = newsapi.get_top_headlines(sources ='techcrunch')
+
+    l = top['articles']
+    desc =[]
+    news =[]
+    img =[]
+
+    for i in range(len(l)):
+        f = l[i]
+        news.append(f['title'])
+        desc.append(f['description'])
+        img.append(f['urlToImage'])
+    mylist = zip(news, desc, img)
+    return render(request, 'newsapp/home.html', context ={"mylist":mylist})
 
 def ContactUs(request):
     return render(request, 'newsapp/contact_us.html')
@@ -28,3 +44,25 @@ def Entertainment(request):
 
 def Business(request):
     return render(request, 'newsapp/business.html')
+
+def index(request):
+    
+    newsapi = NewsApiClient(api_key ='492204b5fab24074b7e237e955eb3218')
+    top = newsapi.get_top_headlines(sources ='techcrunch')
+
+    l = top['articles']
+    desc =[]
+    news =[]
+    img =[]
+
+    for i in range(len(l)):
+        f = l[i]
+        news.append(f['title'])
+        desc.append(f['description'])
+        img.append(f['urlToImage'])
+    mylist = zip(news, desc, img)
+
+
+    return render(request, 'newsapp/index.html', context ={"mylist":mylist})
+
+
