ed72bea05cc9060842596dcdb9f45b3b27d86dec
Akash Shrivastava <akash.shrivastava136@gmail.com>
2023-03-21 12:59:51 +0530

    AS | Add functionality to read secrets from .env

--- newsapp/services.py
+++ newsapp/services.py
@@ -1,8 +1,9 @@
+from django.conf import settings
 from newsapi import NewsApiClient
 
 
 def get_news(country: str, category: str):
-    newsapi = NewsApiClient(api_key="492204b5fab24074b7e237e955eb3218")
+    newsapi = NewsApiClient(api_key=settings.NEWS_API_KEY)
     top_headlines = newsapi.get_top_headlines(country=country, category=category)
 
     top_articles = top_headlines["articles"]
